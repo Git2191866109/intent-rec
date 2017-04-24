e1f4da9922756904eadb3c197d30fb72a09d7f5b
superhy <superhy199148@hotmail.com>
2017-04-24 20:43:50 +0800

    seq2seq_trainer_generator(2017.4.24)

--- core/seq2seq.py
+++ core/seq2seq.py
@@ -10,6 +10,8 @@ from keras.layers.core import Dense, Activation
 from keras.layers.recurrent import LSTM
 from keras.models import Sequential
 from keras.optimizers import RMSprop
+import random
+import sys
 
 import numpy as np
 from recog.embedding import word2Vec
@@ -38,23 +40,22 @@ def onehot_tensorization(text, vocab, vocab_indices):
 
     return x_train, y_train
 
-def w2v_tensorization(text, vocab, vocab_indices, w2v_model):
+def w2v_tensorization(text, vocab, vocab_indices, w2v_model, contLength=10):
     # need input word2vec model for query the word embeddings
     
-    # cut the text in semi-redundant sequences of maxlen characters
-    maxlen = 10
+    # cut the text in semi-redundant sequences of contLength characters
     step = 3
     sentences = []
     next_chars = []
-    for i in range(0, len(text) - maxlen, step):
-        sentences.append(text[i: i + maxlen])
-        next_chars.append(text[i + maxlen])
+    for i in range(0, len(text) - contLength, step):
+        sentences.append(text[i: i + contLength])
+        next_chars.append(text[i + contLength])
     print('nb sequences:', len(sentences))
 
     # vectorization one-hot vector space
     # set up state transfer matrix in tensor space
     print('Word2vec_Tensorization...')
-    x_train = np.zeros((len(sentences), maxlen, len(vocab)), dtype=np.float32)
+    x_train = np.zeros((len(sentences), contLength, len(vocab)), dtype=np.float32)
     y_train = np.zeros((len(sentences), len(vocab)), dtype=np.bool)
     for i, sentence in enumerate(sentences):
         for t, word in enumerate(sentence):
@@ -65,15 +66,12 @@ def w2v_tensorization(text, vocab, vocab_indices, w2v_model):
 
     return x_train, y_train
 
-def LSTM_core(w2v_dim):
-    
-    maxlen = 40
-    
+def LSTM_core(indices_dim, contLength=10):
     # build the model: a single LSTM
     print('Build model...')
     model = Sequential()
-    model.add(LSTM(128, input_shape=(maxlen, w2v_dim)))
-    model.add(Dense(output_dim=w2v_dim))
+    model.add(LSTM(128, input_shape=(contLength, indices_dim)))
+    model.add(Dense(output_dim=indices_dim))
     model.add(Activation('softmax'))
     
     rms_optimizer = RMSprop(lr=0.01)
@@ -90,9 +88,68 @@ def sample(preds, temperature=1.0):
     # e^preds
     exp_preds = np.exp(preds)
     preds = exp_preds / np.sum(exp_preds)
-    probas = np.random.multinomial(1, preds, 1)
+    probas = np.random.multinomial(1, preds, size=1)
     
     return np.argmax(probas)
+
+def trainer():
+    pass
+
+def generator(rnn_model, profix_inputs):
+    pass
+
+def run_generator(corpus, vocab, vocab_indices, indices_vocab,
+                  w2v_model, profix_input, contLength=10):
+    '''
+    need to pre-load the training data include:
+    1. the 
+    '''
+    
+    trainFilePath = '/home/superhy/intent-rec-file/exp_mid_data/train_test-2500/sentences_labeled27500.txt'
+    
+    x_train, y_train = w2v_tensorization(corpus, vocab, vocab_indices)
+    
+    input_dim = len(vocab)
+    
+    model = LSTM_core(indices_dim=input_dim)
+    
+    # train the model, output generated corpus after each iteration
+    for iter in range(1, 20):
+        print()
+        print('-' * 50)
+        
+        print('Iteration', iter)
+        model.fit(x_train, y_train, batch_size=128, nb_epoch=1)  # keras 2.0: nb_epoch changed to epochs
+        
+#         start_index = random.randint(0, len(corpus) - contLength - 1)
+        
+        for diversity in [0.2, 0.5, 1.0, 1.2]:
+            print()
+            print('----- diversity:', diversity)
+            generated = ''
+#             sentence = corpus[start_index: start_index + contLength]
+            sentence = profix_input
+            generated += sentence
+            print('----- Generating with seed: "' + sentence + '"')
+            sys.stdout.write(generated)
+    
+            for i in range(100):
+                x = np.zeros((1, contLength, len(vocab)))
+                for t, word in enumerate(sentence):
+                    vocab_vector = word2Vec.getWordVec(w2v_model, word)
+                    # use Chinese wordvec as the training space
+                    x_train[i, t] = vocab_vector
+    
+                preds = model.predict(x, verbose=0)[0]
+                next_index = sample(preds, diversity)
+                next_char = indices_vocab[next_index]
+    
+                generated += next_char
+                sentence = sentence[1:] + next_char
+    
+                sys.stdout.write(next_char)
+                sys.stdout.flush()
+            print()
 
 if __name__ == '__main__':
     pass
