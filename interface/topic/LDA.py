ffd8d45fe17a7d08a580d0cb6e3adb04ec263569
superhy <superhy@DESKTOP-AMM4OG7>
2017-05-21 02:20:08 +0800

    LDA_start(2017.5.21)

--- interface/topic/LDA.py
+++ interface/topic/LDA.py
@@ -6,5 +6,22 @@ Created on 2017年5月21日
 @author: superhy
 '''
 
+from gensim.models.ldamodel import *
+
+from interface import fileProcess
+
+
+def trainLDA_Model(sentences, modelPath, nb_topics=500):
+    # reload for safe
+    fileProcess.reLoadEncoding()
+    # train lda model
+    lda = LdaModel(corpus=sentences, num_topics=nb_topics, alpha='auto')
+    
+    # save lda model on disk
+    lda.save(fname=modelPath)
+    print('producing lda model ... ok! model store in {0}'.format(modelPath))
+    
+    return lda
+
 if __name__ == '__main__':
     pass
