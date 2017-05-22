e6e50d93f59c16c37fb05527a2713b2659b80b35
superhy <superhy199148@hotmail.com>
2017-05-22 18:06:10 +0800

    test_load_topics(2017.5.22)

--- interface/test/testLDA.py
+++ interface/test/testLDA.py
@@ -7,31 +7,52 @@ Created on 2017.5.21
 '''
 import time
 
-from gensim.models import word2vec
-
 from interface import fileProcess
-from interface.topic.LDA import trainLDA_Model
+from interface.topic.LDA import trainLDA_Model, getAnsweringSentencesfromQAFile,\
+    loadModelfromFile
+from sklearn import lda
 
 
+def getAnsweringSentencesTest():
+    trainPath = fileProcess.auto_config_root() + u'fenke_org/zhongyi_qa_all50000.txt'
+    sentences = getAnsweringSentencesfromQAFile(trainPath)
+    
+    for s in sentences:
+        print(s)
+
+#------------------------------------------------------------------------------ 
+    
 def trainLDA_ModelTest():
     
     fileProcess.reLoadEncoding()
     
     # load all file folder path
-    trainDir = fileProcess.auto_config_root() + u'fenke/zhongyi5000/'
-    
-    files = fileProcess.listAllFilePathInDirectory(trainDir)
-    totalSentences = []
-    for filePath in files:
-        totalSentences.extend(word2vec.LineSentence(filePath))
+    trainDir = fileProcess.auto_config_root() + u'fenke_org/zhongyi_qa_all50000.txt'
+    answeringSentences = getAnsweringSentencesfromQAFile(trainDir)
         
-    ldaModelPath = fileProcess.auto_config_root() + u'model_cache/gensim/zhongyi-5000.topic'
+    ldaModelPath = fileProcess.auto_config_root() + u'model_cache/gensim/zhongyi-lda-50000.topic'
     
     start_lda = time.clock()
-    lda = trainLDA_Model(sentences=totalSentences, modelPath=ldaModelPath, multicore=False)
+    lda = trainLDA_Model(sentences=answeringSentences, modelPath=ldaModelPath, multicore=False)
     end_lda = time.clock()
     print('train gensim lda model finish, use time: {0}'.format(end_lda - start_lda))
     print(lda.num_terms)
     
+def loadModelfromFileTest():
+    
+    ldaModelPath = fileProcess.auto_config_root() + u'model_cache/gensim/zhongyi-lda-50000.topic'
+    lda = loadModelfromFile(modelPath=ldaModelPath)
+    
+    print('number of words: {0}'.format(lda.num_terms))
+    
+    topics = lda.show_topics(num_topics=500, num_words=10, formatted=False)
+    
+    
 if __name__ == '__main__':
-    trainLDA_ModelTest()
+    
+#     getAnsweringSentencesTest()
+
+#     trainLDA_ModelTest()
+    loadModelfromFileTest()
+    
+    
--- interface/topic/LDA.py
+++ interface/topic/LDA.py
@@ -17,7 +17,7 @@ from interface import fileProcess
 def trainLDA_Model(sentences, modelPath, nb_topics=500, multicore=False):
     # reload for safe
     fileProcess.reLoadEncoding()
-    
+
     # load doc2bow
     dictionary = corpora.Dictionary(sentences)
     print('finish load dictionary!')
@@ -25,15 +25,46 @@ def trainLDA_Model(sentences, modelPath, nb_topics=500, multicore=False):
     print('finish load doc2bow corpus!')
     # train lda model
     if multicore == True:
+        # can just use in linux
+        # very hard for CPU, cautiously use it
         lda = LdaMulticore(corpus=corpus, num_topics=nb_topics)
     else:
         lda = LdaModel(corpus=corpus, num_topics=nb_topics)
-    
+
     # save lda model on disk
     lda.save(fname=modelPath)
     print('producing lda model ... ok! model store in {0}'.format(modelPath))
-    
+
     return lda
+
+
+def loadModelfromFile(modelPath, readOnly=False):
+
+    if readOnly == True:
+        lda = LdaModel.load(fname=modelPath, mmap='r')
+    else:
+        lda = LdaModel.load(fname=modelPath)
+    print('load lda model from {0} ok!'.format(modelPath))
+
+    return lda
+
+#------------------------------------------------------------------------------
+
+
+def getAnsweringSentencesfromQAFile(qaFilePath):
+
+    qa_file = open(qaFilePath, 'r')
+    lines = qa_file.readlines()
+    qa_file.close()
+
+    sentences = []
+    for line in lines:
+        if line.find('-') == -1:
+            continue
+        ans_str = line.split('-')[1]
+        sentences.append(ans_str[ans_str.find('[') + 1 : ans_str.find(']')].split(','))
+    
+    return sentences
 
 if __name__ == '__main__':
     pass
